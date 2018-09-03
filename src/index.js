eda3bac717d35611f03cdf97b363e7226b10d8fc
yanghuan <yanghuan@laplacetech.cn>
2018-09-03 15:49:03 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-// import puppeteer from 'puppeteer-core';
+import puppeteer from 'puppeteer-core';
 (async () => {
 	try {
-		const puppeteer = require('puppeteer-core');
 		const browser = await puppeteer.launch({
 			args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
 			// executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
@@ -15,8 +14,26 @@
 			timeout: 300000,
 			waitUntil: 'networkidle0'
 		};
-		await page.goto('https://www.baidu.com/', pageOptions);
+		await page.goto('https://www.udemy.com/topic/react/?ratings=4.5&lang=en&sort=newest', pageOptions);
 		let fullContent = await page.content();
+		let checkElement = await page.$$('[data-purpose="search-course-cards"]');
+		console.log('checkElement.length===========================');
+		console.log(checkElement.length);
+		let courseList = [];
+		if (checkElement) {
+			for (let i = 0; i < checkElement.length; i++) {
+				let signleCourse = {};
+				let item = checkElement[i];
+				console.log('start login item ===========================');
+				let getHrefText = await item.$('a');
+				const courseHref = await page.evaluate((el) => el.href, getHrefText);
+				signleCourse.courseHref = courseHref;
+				console.log(signleCourse);
+				// let getImageElement = await item.$('[class^="list-view-course-card--image-container"]');
+				// console.log(getImageElement);
+				//console.log(item);
+			}
+		}
 
 		await browser.close();
 		return fullContent;
