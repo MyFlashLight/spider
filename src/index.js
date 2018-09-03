438f555c64cf0980d44afe3b49758ab97ef3b7a8
yanghuan <yanghuan@laplacetech.cn>
2018-09-03 13:06:02 +0800

    .

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer-core';
 (async () => {
-	const browser = await puppeteer.launch({
-		args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
-		// executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-		executablePath: '../../chromium/chrome-win32/chrome.exe',
-		//executablePath: 'chromium/chrome-win32/chrome',
-		headless: true,
-		isMobile: true
-	});
-	let page = await browser.newPage();
-	await page.setViewport({ width: 700, height: 800 });
-	let pageOptions = {
-		timeout: timeOut,
-		waitUntil: 'networkidle0'
-	};
-	await page.goto(`${address}`, pageOptions);
-	let fullContent = await page.content();
+	try {
+		const browser = await puppeteer.launch({
+			args: [ '--no-sandbox', '--disable-setuid-sandbox' ],
+			// executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+			executablePath: '../chromium/chrome-win32/chrome.exe',
+			//executablePath: 'chromium/chrome-win32/chrome',
+			headless: false,
+			isMobile: true
+		});
+		let page = await browser.newPage();
+		await page.setViewport({ width: 700, height: 800 });
+		let pageOptions = {
+			timeout: timeOut,
+			waitUntil: 'networkidle0'
+		};
+		await page.goto('https://www.baidu.com/', pageOptions);
+		let fullContent = await page.content();
 
-	await browser.close();
-	return fullContent;
+		await browser.close();
+		return fullContent;
+	} catch (e) {
+		console.log(e);
+	}
 })();
